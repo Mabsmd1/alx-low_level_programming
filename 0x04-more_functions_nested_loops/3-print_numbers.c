a763d379004ed08e1e180efae286d7373e8c63ad
Mabel <moagams10@gmail.com>
2023-02-23 02:43:32 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers 0 through 9
+ * Return: the numbers since 0 up to 9
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
